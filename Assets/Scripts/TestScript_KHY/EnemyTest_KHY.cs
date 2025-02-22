287eca6ce845a94aae5fbf4ee2ecbc8199e26568
cookie35 <hakyeong35@gmail.com>
2025-02-22 21:10:46 +0900

    [feat] 테스트용 스크립트 추가
    던전맵이 잘 돌아가는지 테스트해보기 위한 스크립트

---
+++ Assets/Scripts/TestScript_KHY/EnemyTest_KHY.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTest_KHY : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
