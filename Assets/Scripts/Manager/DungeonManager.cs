a5bf7bdaa53c48cbcb4f89fa8b69d179162cb603
cookie35 <hakyeong35@gmail.com>
2025-02-24 00:02:27 +0900

    [feat] DungeonChanger, ScriptableObject 스크립트 추가

--- Assets/Scripts/Manager/DungeonManager.cs
+++ Assets/Scripts/Manager/DungeonManager.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class DungeonManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static DungeonManager Instance;  // 싱글톤 던전매니저가 전체 1개
+
+    public DungeonSO currentDungeonData;  // ScriptableObject를 불러와 사용
+
+    public void Awake()
     {
-        
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetCurrentMap(DungeonSO dungeonData)
     {
-        
+        currentDungeonData = dungeonData;
     }
+
+    // 랜덤으로 맵 구성하는 기능
+
 }
+
+
+
+    
---
+++ Assets/Scripts/TestScript_KHY/DungeonChanger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cainos.PixelArtTopDown_Basic;
+using UnityEngine;
+
+public class DungeonChanger : MonoBehaviour
+{
+
+    public DungeonSO dungeonSO;
+
+    public TopDownCharacterController playerController;
+
+    public void ChangeDungeon()
+    {
+        DungeonManager.Instance.SetCurrentMap(dungeonSO);  // 맵 데이터 업데이트
+
+        playerController.UpdateBounds();  // 플레이어 이동 범위 업데이트
+    }
+
+
+    private void Awake()
+    {
+        playerController = GameObject.Find("Player").GetComponent<TopDownCharacterController>();
+
+    }
+    private void Start()
+    {
+        ChangeDungeon();
+    }
+
+}
