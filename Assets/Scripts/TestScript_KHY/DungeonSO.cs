65db45ba4e0964acefab497d59e6cdf2e24e78b2
cookie35 <hakyeong35@gmail.com>
2025-02-24 01:16:16 +0900

    [feat] 랜덤뽑기할 Normal Map 여러 개 생성

--- Assets/Scripts/TestScript_KHY/DungeonSO.cs
+++ Assets/Scripts/TestScript_KHY/DungeonSO.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 [CreateAssetMenu]
 public class DungeonSO : ScriptableObject
 {
-    public Vector2 minBounds; // 이동 가능한 최소 x, y 값
-    public Vector2 maxBounds; // 이동 가능한 최대 x, y 값
+    public Vector2 minPlayerBounds; // 플레이어의 이동 가능한 최소 x, y 값
+    public Vector2 maxPlayerBounds; // 플레이어의 이동 가능한 최대 x, y 값
     public float minCameraBounds;  // 카메라 최소 이동 값
     public float maxCameraBounds;  // 카메라 최대 이동 값
 }
