6f906aae77381294e0ee562868e5578e2a9172d6
limewing <58874904+limewing@users.noreply.github.com>
2025-02-24 21:01:47 +0900

    [feat]EquipSlot

--- Assets/Scripts/Manager/InventoryManager.cs
+++ Assets/Scripts/Manager/InventoryManager.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class InventoryManager : MonoBehaviour   // 인벤토리 화면 관리
 {
     public static InventoryManager Instance;
-    public List<Item> displayedItems = new List<Item>();
     public InventoryUI inventoryUI;
+    public Transform inventorySlotGrid;
+    public GameObject itemSlotPrefab;
 
     private void Awake()
     {
@@ -15,22 +16,34 @@ public class InventoryManager : MonoBehaviour   // 인벤토리 화면 관리
 
     public void RefreshInventory()
     {
-        displayedItems = PlayerInventoryManager.Instance.GetOwnedItems();   // 플레이어 아이템 정보 가져와서
-        SortInventoryByRarity();    // 레어도 순서로 정렬
-    }
+        List<Item> items = PlayerInventoryManager.Instance.GetOwnedItems();
 
-    public void SortInventoryByRarity()
-    {
-        displayedItems.Sort((a, b) => CompareRarity(b.rarity, a.rarity));
-        inventoryUI.RefreshInventory(displayedItems);
-    }
-
-    private int CompareRarity(string rarityA, string rarityB)
-    {
-        Dictionary<string, int> rarityOrder = new Dictionary<string, int>
+        // 기존 슬롯 삭제
+        foreach (Transform child in inventorySlotGrid)
         {
-            { "common", 1}, { "rare", 2}, { "unique", 3}, { "legendary", 4}
-        };
-        return rarityOrder[rarityA].CompareTo(rarityOrder[rarityB]);
+            Destroy(child.gameObject);
+        }
+
+        // 새 슬롯 생성
+        foreach (Item item in items)
+        {
+            GameObject slot = Instantiate(itemSlotPrefab, inventorySlotGrid);
+            slot.GetComponent<ItemSlot>().Setup(item);
+        }
+    }
+    public void OnSortByTypeButtonClicked()
+    {
+        List<Item> items = PlayerInventoryManager.Instance.GetOwnedItems();
+        InventorySort inventorySort = GetComponent<InventorySort>();
+        inventorySort.SortByType(items);
+        inventoryUI.RefreshInventory(items);
+    }
+
+    public void OnSortByRarityButtonClicked()
+    {
+        List<Item> items = PlayerInventoryManager.Instance.GetOwnedItems();
+        InventorySort inventorySort = GetComponent<InventorySort>();
+        inventorySort.SortByRarity(items);
+        inventoryUI.RefreshInventory(items);
     }
 }
