d86a5172174a8c31a62b261f9940f69829a6d6de
cookie35 <hakyeong35@gmail.com>
2025-02-24 17:35:50 +0900

    [feat] Collider의 scale 수정
    몬스터가 맵 밖으로 나가지 못하도록 Collider box를 수정하였습니다.

--- Assets/Scripts/Manager/DungeonManager.cs
+++ Assets/Scripts/Manager/DungeonManager.cs
@@ -11,7 +11,7 @@ public class DungeonManager : MonoBehaviour
     public GameObject startDungeon;
     public List<GameObject> normalDungeon;  // 노멀맵 프리팹 리스트
     public List<GameObject> hardDungeon;  // 하드맵 프리팹 리스트
-    public List<GameObject> angelDungeon;  //  엔젤맵 프리팹 리스트 (버프하는 엔젤npc가 있는 맵 1개. 이 맵 포함할지?)
+    // public List<GameObject> angelDungeon;  //  버프하는 엔젤npc가 있는 맵 (이후에 시간 되면 추가)
     public List<GameObject> bossDungeon;  //  던전 프리팹 리스트
 
     private GameObject currentDungeon;  // 현재 활성화된 던전
@@ -19,8 +19,8 @@ public class DungeonManager : MonoBehaviour
 
     public int passedNum = 0;  // 통과한 방의 수를 check하는 넘버
     public int toHardNum = 3;  // passedNum값 안에 들어갈 숫자
-    public int toAngelNum = 5;  // passedNum값 안에 들어갈 숫자
-    public int toBossNum = 6;  // passedNum값 안에 들어갈 숫자
+    // public int toAngelNum = 5;  // passedNum값 안에 들어갈 숫자
+    public int toBossNum = 5;  // passedNum값 안에 들어갈 숫자
 
     public DungeonSO currentDungeonData;  // 현재 던전 데이터. ScriptableObject를 불러와 사용
 
@@ -38,12 +38,10 @@ public class DungeonManager : MonoBehaviour
             Destroy(gameObject);
         }
 
-
-        currentDungeon = Instantiate(startDungeon);  // sartDungeon부터 시작해라
-        currentStage = 0;  // 현재 스테이지 번호
-        passedNum = 0;  // 통과한 방의 수를 check하는 넘버
-        // 핸드폰 화면 꺼놨다가 다시 했을 때 이어하기 되는 기능을 하려면 이 부분이 세이브가 되어야 하는데... 
-        // 어떻게 해야 세이브가 되는 걸까??
+        currentDungeon = Instantiate(startDungeon); 
+        currentStage = 0;  // 현재 스테이지 번호 초기화
+        passedNum = 0;  // 통과한 방의 수를 check하는 넘버를 초기화
+        // 핸드폰 화면 꺼놨다가 다시 했을 때 이어하기 되는 기능을 하려면 이 부분이 세이브가 되어야.
 }
 
     public void SetCurrentMap(DungeonSO dungeonData)  // 현재 던전 데이터. ScriptableObject를 불러와 사용
@@ -83,18 +81,18 @@ public class DungeonManager : MonoBehaviour
                     Debug.LogError("하드맵 프리팹 리스트 비어있음");
                 }
                 break;
-            case 2: // 엔젤 맵 로드
-                if (angelDungeon != null)
-                {
-                    int angelIndex = 0;
-                    currentDungeon = Instantiate(angelDungeon[0]);
-                }
-                else
-                {
-                    Debug.LogError("하드맵 프리팹 리스트 비어있음");
-                }
-                break;
-            case 3: // 보스 맵 로드
+            //case 2: // 엔젤 맵 로드
+            //    if (angelDungeon != null)
+            //    {
+            //        int angelIndex = 0;
+            //        currentDungeon = Instantiate(angelDungeon[0]);
+            //    }
+            //    else
+            //    {
+            //        Debug.LogError("하드맵 프리팹 리스트 비어있음");
+            //    }
+            //    break;
+            case 2: // 보스 맵 로드
                 if (bossDungeon.Count > 0)
                 {
                     int bossIndex = Random.Range(0, bossDungeon.Count);
@@ -116,13 +114,13 @@ public class DungeonManager : MonoBehaviour
         {
             currentStage = 1; 
         }
-        else if (passedNum == toAngelNum)  // 5번 통과하면 angel stage로 넘어감
+        //else if (passedNum == toAngelNum)
+        //{
+        //    currentStage = 2;
+        //}
+        else if (passedNum == toBossNum)  // 5번 통과하면 boss stage로 넘어감
         {
-            currentStage = 2;
-        }
-        else if (passedNum == toBossNum)  // 6번 통과하면 boss stage로 넘어감
-        {
-            currentStage = 3; 
+            currentStage = 2; 
         }
 
     }
