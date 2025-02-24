a8e36ae2cfbeae4fb7fe56da55758c1f26d925b9
limewing <58874904+limewing@users.noreply.github.com>
2025-02-24 09:16:46 +0900

    [Add]Load JsonData

--- Assets/Scripts/Manager/ItemManager.cs
+++ Assets/Scripts/Manager/ItemManager.cs
@@ -16,6 +16,25 @@ public class ItemManager : MonoBehaviour
 
     void LoadItemsFromJson()
     {
-        
+        TextAsset jsonFile = Resources.Load<TextAsset>("ItemData");
+        if (jsonFile == null)
+        {
+            Debug.LogError("ItemData.json 파일이 존재하지 않습니다");
+            return;
+        }
+
+        ItemList itemList = JsonUtility.FromJson<ItemList>(jsonFile.text);
+        foreach (ItemData data in itemList.items)
+        {
+            itemDatabase.Add(new Item(data));
+        }
+
+        Debug.Log($"{itemDatabase.Count}개의 아이템 로드됨");
+    }
+
+    [System.Serializable]
+    public class ItemList
+    {
+        public List<ItemData> items;
     }
 }
