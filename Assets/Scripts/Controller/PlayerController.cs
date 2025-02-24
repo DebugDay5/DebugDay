88815954a7d98a1c08bca60f960c0c68b33d8594
Hwanyi <wksgo123@knu.ac.kr>
2025-02-24 10:52:00 +0900

    [Add] player's look direction to target

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -7,15 +7,19 @@ public class PlayerController : MonoBehaviour
     private PlayerManager playerManager;
 
     private Rigidbody2D _rigidbody;
+    private SpriteRenderer _spriteRenderer;
 
     private Vector2 moveDirection; //이동 방향
     private Vector2 lookDirection;  //발사 방향
 
-    private GameObject target; //발사할 적
+    public Transform[] target; //발사할 적
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+
         moveDirection = Vector2.zero;
         lookDirection = Vector2.zero;
     }
@@ -38,6 +42,7 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         Movement(moveDirection);
+        AttackTarget();
     }
 
     private void Movement(Vector2 moveDirection)
@@ -49,9 +54,27 @@ public class PlayerController : MonoBehaviour
 
     private void AttackTarget()
     {
-        if(target == null) return; //적이 없다면
-        
-        lookDirection = target.transform.position - transform.position;
+        if(target.Length == 0) return; //적이 없다면
+
+        Transform go = target[0];
+        float min = Mathf.Infinity;
+
+        foreach(var t in target)
+        {
+            float distance = (t.position - transform.position).magnitude;
+            if (distance < min)
+            {
+                min = distance;
+                go = t;
+            }
+        }
+
+        lookDirection = go.position - transform.position;
         lookDirection = lookDirection.normalized;
+
+        float rotZ = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        bool isLeft = Mathf.Abs(rotZ) > 90f;
+        _spriteRenderer.flipX = isLeft;
+
     }
 }
