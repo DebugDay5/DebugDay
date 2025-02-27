7bf105f8fac28b492e05ec24e624c5341722b981
cookie35 <hakyeong35@gmail.com>
2025-02-27 10:57:15 +0900

    [fix] 적이 inspector창에서 안 보이는 문제 수정

--- Assets/Scripts/Dungeon/DungeonChanger.cs
+++ Assets/Scripts/Dungeon/DungeonChanger.cs
@@ -6,16 +6,14 @@ using UnityEngine;
 public class DungeonChanger : MonoBehaviour
 {
     public DungeonSO dungeonSO;
-
     public PlayerController playerController;
     public TestCamera_KHY cameraController;
 
-    public GameObject[] enemys;
+    public GameObject[] enemys;  // 적 수동할당 가능
 
     public void ChangeDungeon()
     {
         DungeonManager.Instance.SetCurrentMap(dungeonSO);  // 맵 데이터 업데이트
-
         playerController.UpdateBounds();  // 플레이어 이동 범위 업데이트
         cameraController.CameraUpdateBounds();  // 카메라 이동 범위 업데이트
     }
@@ -24,8 +22,17 @@ public class DungeonChanger : MonoBehaviour
     {
         playerController = GameObject.Find("Player").GetComponent<PlayerController>();
         cameraController = Camera.main.GetComponent<TestCamera_KHY>();
-        PlayerManager.Instance.target = new GameObject[enemys.Length];
-        PlayerManager.Instance.target = enemys;
+
+        if (enemys == null ||  enemys.Length == 0 )  // 수동 할당이 없을 경우 적 오브젝트 자동 탐색
+        {
+            enemys = GameObject.FindGameObjectsWithTag("Enemy");
+        }
+
+        if (PlayerManager.Instance != null)
+        {
+            PlayerManager.Instance.target = enemys;
+        }
+
     }
     private void Start()
     {
